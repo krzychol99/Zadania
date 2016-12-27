0db70e5c706d777e59fa9a751305332d4ae0071f
krzychol99 <krzysztof.odachowski@gmail.com>
2016-12-27 01:20:53 +0100

    Initial commit

---
+++ #zajęcia3_zadanie5/src/TriangleTest.java
@@ -0,0 +1,19 @@
+/**
+ * Created by krzychol99 on 2016-12-26.
+ */
+public class TriangleTest {
+    public static void main(String[] args) {
+
+        Triangle triangle0 = new Triangle(4, 3, 5);
+        Triangle triangle1 = new Triangle(2,3,7);
+        Triangle triangle2 = new Triangle(3,4,5);
+        Triangle triangle3 = new Triangle(5,2,13);
+
+        TriangleChecker triangleChecker = new TriangleChecker();
+
+        System.out.println(triangleChecker.isRightTriangle(triangle0));
+        System.out.println(triangleChecker.isRightTriangle(triangle1));
+        System.out.println(triangleChecker.isRightTriangle(triangle2));
+        System.out.println(triangleChecker.isRightTriangle(triangle3));
+    }
+}
