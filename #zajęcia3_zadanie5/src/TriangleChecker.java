d2f755c0f5ed6af6a2425a12406f03fc3ce8f130
krzychol99 <krzysztof.odachowski@gmail.com>
2016-12-27 02:32:43 +0100

    Update isRightTriangle method

--- #zajęcia3_zadanie5/src/TriangleChecker.java
+++ #zajęcia3_zadanie5/src/TriangleChecker.java
@@ -7,9 +7,9 @@ public class TriangleChecker {
         double powSide_C = Math.pow(triangle.sideC, 2);
         double powSide_A_B = Math.pow(triangle.sideA, 2) + Math.pow(triangle.sideB, 2);
 
-        if (((triangle.sideA + triangle.sideB > triangle.sideC)
-                || (triangle.sideA + triangle.sideC > triangle.sideB)
-                || (triangle.sideB + triangle.sideC > triangle.sideA)== true)
+        if (((triangle.sideA + triangle.sideB >= triangle.sideC)
+                || (triangle.sideA + triangle.sideC >= triangle.sideB)
+                || (triangle.sideB + triangle.sideC >= triangle.sideA)== true)
                 && (powSide_C == powSide_A_B) == true){
 
             return true;
