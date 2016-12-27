303d115f2954ec5608d6b5464151d238923408ab
krzychol99 <krzysztof.odachowski@gmail.com>
2016-12-27 02:34:24 +0100

    Update TiangleTest.class

--- #zajęcia3_zadanie5/src/TriangleTest.java
+++ #zajęcia3_zadanie5/src/TriangleTest.java
@@ -7,13 +7,13 @@ public class TriangleTest {
         Triangle triangle0 = new Triangle(4, 3, 5);
         Triangle triangle1 = new Triangle(2,3,7);
         Triangle triangle2 = new Triangle(3,4,5);
-        Triangle triangle3 = new Triangle(5,2,13);
+        Triangle triangle3 = new Triangle(3,3,3);
 
         TriangleChecker triangleChecker = new TriangleChecker();
 
-        System.out.println(triangleChecker.isRightTriangle(triangle0));
-        System.out.println(triangleChecker.isRightTriangle(triangle1));
-        System.out.println(triangleChecker.isRightTriangle(triangle2));
-        System.out.println(triangleChecker.isRightTriangle(triangle3));
+        System.out.println("Triangle 1st is right-angled: " + triangleChecker.isRightTriangle(triangle0));
+        System.out.println("Triangle 2nd is right-angled: " + triangleChecker.isRightTriangle(triangle1));
+        System.out.println("Triangle 3rd is right-angled: " + triangleChecker.isRightTriangle(triangle2));
+        System.out.println("Triangle 4th is right-angled: " + triangleChecker.isRightTriangle(triangle3));
     }
 }
