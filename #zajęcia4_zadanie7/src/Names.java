6b8c264fc92c05fe6cf888a4fe2e57b4e22a0691
krzychol99 <krzysztof.odachowski@gmail.com>
2017-01-09 17:30:26 +0100

    Initial commit

---
+++ #zajęcia4_zadanie7/src/Names.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * Created by krzychol99 on 2017-01-08.
+ */
+public class Names {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String[] names = new String[5];
+        String helloNames = "Hello ";
+        int i = 0;
+        System.out.println("Enter 5 names: ");
+
+        while (i < names.length ){
+            names[i] = input.nextLine();
+            i++;
+        }
+        int counter = names.length - 1;
+        while(counter<names.length && counter >= 0){
+            System.out.println(helloNames + " " + names[counter]);
+            counter--;
+        }
+    }
+
+}
