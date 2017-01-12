c628299e4d5bf0d2fd596d2a09e4a634f0eea506
krzychol99 <krzysztof.odachowski@gmail.com>
2017-01-12 20:00:59 +0100

    Initial commit

--- #zajęcia5_zadanie1_Static/src/ConverterTest.java
+++ #zajęcia5_zadanie1_Static/src/ConverterTest.java
@@ -3,13 +3,12 @@
  */
 public class ConverterTest {
     public static void main(String[] args) {
-        CalendarConverter converter = new CalendarConverter();
 
         for (int i = 1; i<8; i++){
             String descriptionDay = " dzień tygodnia to: ";
 
             System.out.println(i + descriptionDay);
-            converter.convertDayToString(i);
+            CalendarConverter.convertDayToString(i);
         }
     }
 }
