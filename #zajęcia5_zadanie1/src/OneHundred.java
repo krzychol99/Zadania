149554a747f408caf7142ca27f29d8f31bf5bcea
krzychol99 <krzysztof.odachowski@gmail.com>
2017-01-12 16:21:58 +0100

    Initial commit

---
+++ #zajęcia5_zadanie1/src/OneHundred.java
@@ -0,0 +1,13 @@
+/**
+ * Created by krzychol99 on 2017-01-11.
+ */
+public class OneHundred {
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 100; i++) {
+            if(i%3 == 0 && i%5 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
